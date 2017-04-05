31f5884757d907aab17a5a0183682a6ebac7773c
Emiliano Borzelli <vibrains@gmail.com>
2017-04-05 16:26:17 -0400

    created sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,9 @@
 <div class="col-md-4 col-md-offset-1">
 	<div id="sidebar" role="complementary">
-		<div id="sidebar">
-			<ul>
-				<?php
-				if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('homepage-sidebar') ) :
-					endif; ?>
-			</ul>
-		</div>
+		<ul>
+			<?php
+			if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('events-sidebar') ) :
+				endif; ?>
+		</ul>
 	</div>
 </div>
