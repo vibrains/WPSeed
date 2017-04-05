5fd3b75b518c98bc0ef838a142f7269321453eb3
Emiliano Borzelli <vibrains@gmail.com>
2017-04-05 16:29:26 -0400

    worked on blogroll page and single post page

--- templates/wp-home.php
+++ templates/wp-home.php
@@ -10,22 +10,41 @@
 ?>
 
 <?php get_header(); ?>
-
 <!-- content » blog home -->
-
 <div class="content blog">
+ <div class="container" id="main">
+  <div class="row">
+    <h1 class="col-md-12 page-title">Blog</h1>
+    <?php if (have_posts() ) : while (have_posts()) : the_post(); ?>
+     <article class="page col-md-7" id="post-<?php the_ID(); ?>">
+      <a href="<?php echo get_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+      <div class="post-info">
+       <span class="entry-date"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;<?php echo get_the_date(); ?></span><span class="entry-author">By:&nbsp;<?php echo get_the_author(); ?></span>
+     </div>
 
-  <?php if (have_posts() ) : while (have_posts()) : the_post(); ?>
-    <article>
-      <h2><?php the_title(); ?></h2>
-      <div class="postinfo"><?php wpseed_get_the_date_german(); ?></div>
-      <?php if (has_post_thumbnail()) : ?>
-        <div class="thumbnail" style="background-image: url(<?php the_post_thumbnail_url() ?>)"></div>
-      <?php endif ?>
-      <?php the_content(); ?>
-    </article>
-  <?php endwhile; endif; ?>
+     <p>
+       <?php do_action( 'addthis_widget', array(
+    'size' => '16', // size of the icons.  Either 16 or 32
+    'services' => 'facebook,joliprint', // the services you want to always appear
+    'preferred' => '0', // the number of auto personalized services
+    'more' => false // if you want to have a more button at the end
+    ));
+    ?>
+  </p>
 
+  <?php if (has_post_thumbnail()) : ?>
+    <div class="thumbnail" style="background-image: url(<?php the_post_thumbnail_url() ?>)"></div>
+  <?php endif ?>
+  <p>
+    <?php
+    echo wp_trim_words( get_the_content(), 32, '...' );
+    ?></p>
+    <a class="read-more-link button" href="<?php echo get_permalink(); ?>">Read More&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+  </article>
+  <?php get_sidebar(); ?>
+<?php endwhile; endif; ?>
+<?php wp_reset_query(); ?>
 </div>
-
+</div>
+</div>
 <?php get_footer(); ?>
--- templates/wp-single.php
+++ templates/wp-single.php
@@ -13,21 +13,22 @@
 <!-- content » single post -->
 
 <div class="content post">
-  <?php if (have_posts() ) : while (have_posts()) : the_post(); ?>
-  <article>
-    <div class="col-xs-8">
-      <h2><?php the_title(); ?></h2>
+ <div class="container" id="main">
+  <div class="row">
+    <?php if (have_posts() ) : while (have_posts()) : the_post(); ?>
+      <article>
+        <div class="col-md-7">
+          <h2><?php the_title(); ?></h2>
 
-      <?php if (has_post_thumbnail()) : ?>
-      <div class="thumbnail" style="background-image: url(<?php the_post_thumbnail_url() ?>)"></div>
-    <?php endif ?>
-    <?php the_content(); ?>
+          <?php if (has_post_thumbnail()) : ?>
+            <div class="thumbnail" style="background-image: url(<?php the_post_thumbnail_url() ?>)"></div>
+          <?php endif ?>
+          <?php the_content(); ?>
+        </div>
+      </article>
+    <?php endwhile; endif; ?>
+    <?php get_sidebar(); ?>
   </div>
-</article>
-<?php endwhile; endif; ?>
-
-<div class="col-xs-3 col-xs-offset-1">
-  <?php get_sidebar(); ?>
 </div>
 </div>
 <?php get_footer(); ?>
