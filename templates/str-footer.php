cdf08b7adb60b9633cce8856c1bb170e08ae29fb
Emiliano Borzelli <vibrains@gmail.com>
2017-04-10 17:40:29 -0400

    added footer content and styles

--- templates/str-footer.php
+++ templates/str-footer.php
@@ -4,42 +4,39 @@
 			<article class="content home our-partners">
 				<div class="col-xs-12">
 					<span class="section-title text-center">Our Partners</span>
-<!-- 					<ul>
-						<li>
-							<img src="/wp-content/themes/wpseed/dist/images/partners/hot-107-9-atlanta-logo.png" alt="Hot 107.9 Atlanta">
-							<img src="/wp-content/themes/wpseed/dist/images/partners/kantar-media-logo.png" alt="Kantar Media">
-							<img src="/wp-content/themes/wpseed/dist/images/partners/radio-one-logo.png" alt="Radio One">
-							<img src="/wp-content/themes/wpseed/dist/images/partners/gen-media-partners-logo.jpg" alt="Gen Media Partners">
-							<img src="/wp-content/themes/wpseed/dist/images/partners/kicks-fm-logo.png" alt="Kick FM">
-							<img src="/wp-content/themes/wpseed/dist/images/partners/ninety-nine-x-logo.png" alt="Ninety Nine X">
-						</li>
-					</ul> -->
-
-					<?php echo do_shortcode( '[logoshowcase]' ); ?>
-
+					<?php //echo do_shortcode( '[logoshowcase]' ); ?>
 				</div>
 			</article>
 		</div>
 
-		<div class="content footer-content">
-			<div class="row">
-				<div class="col-md-4 col-xs-12">
-					<div class="footer-section-title">
-						<span>Upcoming Events</span>
-					</div>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, pariatur nostrum! Repellat saepe eum sequi unde, qui consequatur, ea aut, nobis laboriosam corrupti reprehenderit! Laborum distinctio, cumque nesciunt minus at.</p>
+		<div id="footer-sidebar" class="secondary">
+			<div class="col-md-4 col-xs-12">
+				<div id="footer-sidebar1">
+					<?php
+					if(is_active_sidebar('footer-sidebar-1')){
+						dynamic_sidebar('footer-sidebar-1');
+					}
+					?>
 				</div>
-				<div class="col-md-4 col-xs-12">
-					<div class="footer-section-title">
-						<span>Quick Links</span>
-					</div>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem est quisquam tempora. Porro at cupiditate, ducimus nam ipsa nisi fuga minima magni delectus eligendi aspernatur, voluptas amet error voluptatibus! Placeat?</p>
+			</div>
+
+			<div class="col-md-4 col-xs-12">
+				<div id="footer-sidebar2">
+					<?php
+					if(is_active_sidebar('footer-sidebar-2')){
+						dynamic_sidebar('footer-sidebar-2');
+					}
+					?>
 				</div>
-				<div class="col-md-4 col-xs-12">
-					<div class="footer-section-title">
-						<span>Contact Us</span>
-					</div>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic beatae quae vel quo. Ex vitae ipsum, accusantium voluptas quos suscipit vero laborum quis velit expedita nemo atque eligendi at libero.</p>
+			</div>
+
+			<div class="col-md-4 col-xs-12">
+				<div id="footer-sidebar3">
+					<?php
+					if(is_active_sidebar('footer-sidebar-3')){
+						dynamic_sidebar('footer-sidebar-3');
+					}
+					?>
 				</div>
 			</div>
 		</div>
