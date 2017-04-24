92bdbf290cae30f6bdfe287d7b89cb6d55223a68
Emiliano Borzelli <vibrains@gmail.com>
2017-04-24 12:39:25 -0400

    added the content function to staff page

--- templates/staff.php
+++ templates/staff.php
@@ -7,37 +7,55 @@ get_header(); ?>
 <!-- <div class="events-page teaser" style="max-height: 300px; background-repeat:no-repeat!important; background-size:cover!important; background: url(<?php the_post_thumbnail_url('large', $post->ID) ?>)"></div> -->
 <div class="page-holder container staff-page" id="main">
 	<div class="row">
-		<h1 class="col-md-12 page-title"><i class="fa fa-users" aria-hidden="true"></i>&nbsp;&nbsp;The ABAC Board Members and Officers</h1>
-		<h2 class="col-md-12 staff-section-title">The Officers</h2>
-		<?php
-		if( have_rows('officers') ):
-			while ( have_rows('officers') ) : the_row();
-		$image = get_sub_field('officers_photo');
-		$name = get_sub_field('officers_name');
-		$position = get_sub_field('officers_position');
-		$biography = get_sub_field('officers_bio');
-		$email = get_sub_field('officers_mail');
-		$linkedin = get_sub_field('officers_linkedin');
-		$twitter = get_sub_field('officers_twitter');
-		?>
-		<div class="col-xs-12 col-sm-6 col-md-3">
-			<div class="panel panel-default staff">
-				<div class="panel-body">
-					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-					<div class="staff-content">
-						<p class="staff-name"><?php the_sub_field('officers_name'); ?></p>
-						<div class="col-md-12 no-padding">
-							<div class="staff-position">
-								<p><?php the_sub_field('officers_position'); ?></p>
-							</div>
-							<div class="staff-bio">
-								<p class="staff-bio"><?php the_sub_field('officers_bio'); ?></p>
-							</div>
+
+		<div class="row">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<h1 class="col-md-12 page-title"><i class="fa fa-users" aria-hidden="true"></i>&nbsp;&nbsp;The ABAC Board Members and Officers</h1>
+				<article class="page col-md-12" id="post-<?php the_ID(); ?>">
+					<div class="entry">
+						<?php the_content(); ?>
+					</div>
+				</article>
+			<?php endwhile; else: ?>
+			<article class="page col-md-7 not-found">
+				<div class="entry">
+					<p class="lead"><?php _e('Sorry, this page does not exist. Try searching for one.'); ?></p>
+					<?php get_search_form(); ?>
+				</div>
+			</article>
+		<?php endif; ?>
+	</div> <!-- .row -->
+
+	<h2 class="col-md-12 staff-section-title">The Officers</h2>
+	<?php
+	if( have_rows('officers') ):
+		while ( have_rows('officers') ) : the_row();
+	$image = get_sub_field('officers_photo');
+	$name = get_sub_field('officers_name');
+	$position = get_sub_field('officers_position');
+	$biography = get_sub_field('officers_bio');
+	$email = get_sub_field('officers_mail');
+	$linkedin = get_sub_field('officers_linkedin');
+	$twitter = get_sub_field('officers_twitter');
+	?>
+	<div class="col-xs-12 col-sm-6 col-md-3">
+		<div class="panel panel-default staff">
+			<div class="panel-body">
+				<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+				<div class="staff-content">
+					<p class="staff-name"><?php the_sub_field('officers_name'); ?></p>
+					<div class="col-md-12 no-padding">
+						<div class="staff-position">
+							<p><?php the_sub_field('officers_position'); ?></p>
+						</div>
+						<div class="staff-bio">
+							<p class="staff-bio"><?php the_sub_field('officers_bio'); ?></p>
 						</div>
 					</div>
-					<div class="col-xs-12 no-padding staff-social-links">
-						<a class="button" href="mailto:<?php the_sub_field('officers_mail'); ?>"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;&nbsp;Send Email</a>
-					</div>
+				</div>
+				<div class="col-xs-12 no-padding staff-social-links">
+					<a class="button" href="mailto:<?php the_sub_field('officers_mail'); ?>"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp;&nbsp;Send Email</a>
+				</div>
 
 
 <!--<a href="<?php the_sub_field('officers_linkedin'); ?>" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
